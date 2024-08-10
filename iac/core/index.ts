9dac5dcf79495a7ec011f9cd85c8e9a178bd7aed
Umer Naeem <25100029@lums.edu.pk>
2024-08-10 14:48:54 +0500

    feat: :card_file_box: made db publicly accessble + docker command for connection

--- iac/core/index.ts
+++ iac/core/index.ts
@@ -10,6 +10,17 @@ const password = new random.RandomPassword("db-password", {
     special: false,
 })
 
+// Create a Security Group
+const dbSecurityGroup = new aws.ec2.SecurityGroup("mydbSg", {
+    description: "Allow inbound traffic",
+    ingress: [{
+        fromPort: 5432,
+        toPort: 5432,
+        protocol: "tcp",
+        cidrBlocks: ["0.0.0.0/0"],
+    }],
+});
+
 // Create Database Instance
 const databaseInstance = new aws.rds.Instance("main-db", {
     // Metadata
@@ -34,6 +45,10 @@ const databaseInstance = new aws.rds.Instance("main-db", {
 
     // Monitoring
     enabledCloudwatchLogsExports: ["postgresql"],
+
+    // Configure Networking and Firewalls
+    publiclyAccessible: true,
+    vpcSecurityGroupIds: [dbSecurityGroup.id],
 });
 
 const appIamUser = new aws.iam.User("app-iam-user", {
@@ -51,5 +66,9 @@ export const appIamAccessKey = appIamUserKey.secret;
 
 // exporting Database details
 export const databaseInstanceName = databaseInstance.identifier;
+export const databaseHost = databaseInstance.address;
 export const databaseUsername = databaseInstance.username;
 export const databasePassword = databaseInstance.password;
+
+// Docker command to connect to PostgreSQL using Alpine-based image
+export const connectionCommand = pulumi.interpolate`docker run --rm -it -e PGPASSWORD=${databasePassword} postgres:${databaseInstance.engineVersion}-alpine psql -h ${databaseHost} -U ${databaseUsername}`;
