e024b7f120a4e05d326b06a80e4f7d078611a8ec
Umer Naeem <25100029@lums.edu.pk>
2024-08-10 15:05:41 +0500

    prettier consistent formatting -- ignore file changes

--- iac/app/index.ts
+++ iac/app/index.ts
@@ -1,46 +1,46 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from '@pulumi/pulumi'
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
 
 // Environment Variables
-const awsConfig = new pulumi.Config("aws");
-const region = awsConfig.require("region");
+const awsConfig = new pulumi.Config('aws')
+const region = awsConfig.require('region')
 
 // Reference to Stack Outputs For Parent Stack
-const coreStackReference = new pulumi.StackReference(`organization/core-app/default`);
+const coreStackReference = new pulumi.StackReference(`organization/core-app/default`)
 
-const databaseInstanceName = coreStackReference.requireOutput("databaseInstanceName");
-const databaseUsername = coreStackReference.requireOutput("databaseUsername");
-const databasePassword = coreStackReference.requireOutput("databasePassword");
+const databaseInstanceName = coreStackReference.requireOutput('databaseInstanceName')
+const databaseUsername = coreStackReference.requireOutput('databaseUsername')
+const databasePassword = coreStackReference.requireOutput('databasePassword')
 
-const appIamUserName = coreStackReference.requireOutput("appIamUserName");
-const appIamAccessId = coreStackReference.requireOutput("appIamAccessId");
-const appIamAccessKey = coreStackReference.requireOutput("appIamAccessKey");
+const appIamUserName = coreStackReference.requireOutput('appIamUserName')
+const appIamAccessId = coreStackReference.requireOutput('appIamAccessId')
+const appIamAccessKey = coreStackReference.requireOutput('appIamAccessKey')
 
 // Using the Stack Name as The Environment Name
-const environment = pulumi.getStack();
+const environment = pulumi.getStack()
 
 /**
  * We Would be creating a Docker Repository for storing our application packages.
- * This repository is for the Core Server Application. 
+ * This repository is for the Core Server Application.
  */
-const coreServerDockerRepo = new aws.ecr.Repository("core-server-docker-repo", {
+const coreServerDockerRepo = new aws.ecr.Repository('core-server-docker-repo', {
     name: `awaaz-core-server-${environment}`,
 
     // For Metadata Purposes
     tags: {
-        ProjectName: "awaaz-core-server",
+        ProjectName: 'awaaz-core-server',
         EnvironmentType: environment,
     },
-});
+})
 
 /**
  * Let's also create a S3 Bucket for Storing any kind of files like:
  * - EMR Audio Files
  * - Big data blobs
  */
-const uploadsBucket = new aws.s3.Bucket("uploads-bucket", {
-    bucket: `awaaz-sehat-uploads-${environment}`
+const uploadsBucket = new aws.s3.Bucket('uploads-bucket', {
+    bucket: `awaaz-sehat-uploads-${environment}`,
 })
 
 /**
@@ -48,30 +48,30 @@ const uploadsBucket = new aws.s3.Bucket("uploads-bucket", {
  */
 // const coreServerAppRunnerConfig = new aws.apprunner.Auto("core-server-")
 
-const coreServerAppRunnerService = new aws.apprunner.Service("core-server-service", {
+const coreServerAppRunnerService = new aws.apprunner.Service('core-server-service', {
     serviceName: `awaaz-core-server-${environment}`,
     sourceConfiguration: {
         imageRepository: {
             imageConfiguration: {
-                port: "8000",
+                port: '8000',
                 runtimeEnvironmentSecrets: {},
                 runtimeEnvironmentVariables: {},
             },
             // TODO: It should use the reference to our core service repository
-            imageIdentifier: "public.ecr.aws/aws-containers/hello-app-runner:latest",
-            imageRepositoryType: "ECR_PUBLIC",
+            imageIdentifier: 'public.ecr.aws/aws-containers/hello-app-runner:latest',
+            imageRepositoryType: 'ECR_PUBLIC',
         },
         // should be set to true
         autoDeploymentsEnabled: false,
     },
     instanceConfiguration: {
-        cpu: "256",
-        memory: "512",
+        cpu: '256',
+        memory: '512',
         // This might be equivalent to the GCP Service Account but i am not sure.
         // instanceRoleArn: "???",
-    }
-});
+    },
+})
 
-export const coreServerRepo = coreServerDockerRepo.name;
-export const coreServiceUrl = coreServerAppRunnerService.serviceUrl;
-export const uploadBucketName = uploadsBucket.bucket;
+export const coreServerRepo = coreServerDockerRepo.name
+export const coreServiceUrl = coreServerAppRunnerService.serviceUrl
+export const uploadBucketName = uploadsBucket.bucket
--- iac/core/index.ts
+++ iac/core/index.ts
@@ -1,74 +1,76 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
-import * as random from "@pulumi/random";
+import * as pulumi from '@pulumi/pulumi'
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+import * as random from '@pulumi/random'
 
-const awsConfig = new pulumi.Config("aws");
+const awsConfig = new pulumi.Config('aws')
 
-const password = new random.RandomPassword("db-password", {
+const password = new random.RandomPassword('db-password', {
     length: 24,
     special: false,
 })
 
 // Create a Security Group
-const dbSecurityGroup = new aws.ec2.SecurityGroup("mydbSg", {
-    description: "Allow inbound traffic",
-    ingress: [{
-        fromPort: 5432,
-        toPort: 5432,
-        protocol: "tcp",
-        cidrBlocks: ["0.0.0.0/0"],
-    }],
-});
+const dbSecurityGroup = new aws.ec2.SecurityGroup('mydbSg', {
+    description: 'Allow inbound traffic',
+    ingress: [
+        {
+            fromPort: 5432,
+            toPort: 5432,
+            protocol: 'tcp',
+            cidrBlocks: ['0.0.0.0/0'],
+        },
+    ],
+})
 
 // Create Database Instance
-const databaseInstance = new aws.rds.Instance("main-db", {
+const databaseInstance = new aws.rds.Instance('main-db', {
     // Metadata
-    identifier: "awaaz-sehat-main",
-    username: "postgres",
+    identifier: 'awaaz-sehat-main',
+    username: 'postgres',
     password: password.result,
 
     // Database Engine Details
-    engine: "postgres",
-    engineVersion: "14.10",
+    engine: 'postgres',
+    engineVersion: '14.10',
 
     // Compute Capacity
     instanceClass: aws.rds.InstanceType.T3_Micro,
 
     // Storage Capacity
     allocatedStorage: 32,
-    storageType: "gp2",
+    storageType: 'gp2',
 
     // Backup Configuration
     backupRetentionPeriod: 7,
     deleteAutomatedBackups: false,
 
     // Monitoring
-    enabledCloudwatchLogsExports: ["postgresql"],
+    enabledCloudwatchLogsExports: ['postgresql'],
 
     // Configure Networking and Firewalls
     publiclyAccessible: true,
     vpcSecurityGroupIds: [dbSecurityGroup.id],
-});
+})
 
-const appIamUser = new aws.iam.User("app-iam-user", {
-    name: "awaaz-app-user"
-});
+const appIamUser = new aws.iam.User('app-iam-user', {
+    name: 'awaaz-app-user',
+})
 
-const appIamUserKey = new aws.iam.AccessKey("core-app-iam-access-key", {
+const appIamUserKey = new aws.iam.AccessKey('core-app-iam-access-key', {
     user: appIamUser.name,
 })
 
 // Exporting Name of User, Access ID, and Access Secret
-export const appIamUserName = appIamUser.name;
-export const appIamAccessId = appIamUserKey.id;
-export const appIamAccessKey = appIamUserKey.secret;
+export const appIamUserName = appIamUser.name
+export const appIamAccessId = appIamUserKey.id
+export const appIamAccessKey = appIamUserKey.secret
 
 // exporting Database details
-export const databaseInstanceName = databaseInstance.identifier;
-export const databaseHost = databaseInstance.address;
-export const databaseUsername = databaseInstance.username;
-export const databasePassword = databaseInstance.password;
+export const databaseInstanceName = databaseInstance.identifier
+export const databaseHost = databaseInstance.address
+export const databaseUsername = databaseInstance.username
+export const databasePassword = databaseInstance.password
 
 // Docker command to connect to PostgreSQL using Alpine-based image
-export const connectionCommand = pulumi.interpolate`docker run --rm -it -e PGPASSWORD=${databasePassword} postgres:${databaseInstance.engineVersion}-alpine psql -h ${databaseHost} -U ${databaseUsername}`;
+export const connectionCommand = pulumi.interpolate`docker run --rm -it -e PGPASSWORD=${databasePassword} postgres:${databaseInstance.engineVersion}-alpine psql -h ${databaseHost} -U ${databaseUsername}`
