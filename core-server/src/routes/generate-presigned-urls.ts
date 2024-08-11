d8781873c04e76324e97dcc987f8061cb83352a5
Umer Naeem <25100029@lums.edu.pk>
2024-08-12 04:35:31 +0500

    signed url route added

---
+++ core-server/src/routes/generate-presigned-urls.ts
@@ -0,0 +1,70 @@
+import app from '@/app'
+import { createRoute, z } from '@hono/zod-openapi'
+import { createPresignedPutUrl, createPresignedGetUrl } from '@/services/storage'
+import { ulid } from 'ulidx'
+
+// Define the request schema to include the extension
+const GeneratePresignedUrlsRequestSchema = z.object({
+    extension: z.enum(['mp3', 'wav', 'm4a', 'mpeg', 'webm', 'mpga', 'mp4']).openapi({
+        example: 'mp3',
+    }),
+})
+
+// Define the response schema
+const GeneratePresignedUrlsResponseSchema = z.object({
+    fileId: z.string().openapi({
+        example: '01F8MECHZX3TBDSZ7XRADM79XE.mp3',
+    }),
+    uploadUrl: z.string().openapi({
+        example:
+            'https://storage.googleapis.com/bucket/01F8MECHZX3TBDSZ7XRADM79XE.mp3?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=...',
+    }),
+    downloadUrl: z.string().openapi({
+        example:
+            'https://storage.googleapis.com/bucket/01F8MECHZX3TBDSZ7XRADM79XE.mp3?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=...',
+    }),
+})
+
+// Create the route
+const route = createRoute({
+    method: 'post',
+    operationId: 'generatePresignedUrls',
+    tags: ['Storage'],
+    path: '/storage/generate-urls',
+    summary:
+        'Generate an ID and provide presigned URLs for uploading and downloading a file. The download URL is used to retrieve the uploaded file.',
+    request: {
+        query: GeneratePresignedUrlsRequestSchema,
+    },
+    responses: {
+        200: {
+            content: {
+                'application/json': {
+                    schema: GeneratePresignedUrlsResponseSchema,
+                },
+            },
+            description: 'Presigned URLs generated successfully',
+        },
+    },
+})
+
+// Define the handler
+const generatePresignedUrlsHandler = app.openapi(route, async (c) => {
+    const { extension } = c.req.valid('query')
+    const id = ulid()
+    const bucket = process.env.UPLOAD_BUCKET || 'undefined'
+    const key = `${id}.${extension}`
+    const uploadUrl = await createPresignedPutUrl({ bucket, key })
+    const downloadUrl = await createPresignedGetUrl({ bucket, key })
+    return c.json(
+        {
+            fileId: key,
+            uploadUrl,
+            downloadUrl,
+        },
+        200
+    )
+})
+
+export type GeneratePresignedUrlsRoute = typeof generatePresignedUrlsHandler
+export default route
--- core-server/src/routes/index.ts
+++ core-server/src/routes/index.ts
@@ -2,9 +2,13 @@ import exampleRoute from '@/routes/example'
 import homeRoute from '@/routes/home-page'
 import loginDoctorRoute from '@/routes/login-doctor'
 import registerDoctorRoute from '@/routes/register-doctor'
+import generatePresignedUrls from '@/routes/generate-presigned-urls'
 
 export const registerRoutes = () => {
+    // hacky way to call the scripts and registering the routes
     exampleRoute.getRoutingPath()
     homeRoute.getRoutingPath()
+    loginDoctorRoute.getRoutingPath()
     registerDoctorRoute.getRoutingPath()
+    generatePresignedUrls.getRoutingPath()
 }
