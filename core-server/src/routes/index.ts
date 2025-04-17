4664dd419d8ef44e727aae6f4cbb6547893b0ead
Wahaj Khalid <wahajkhalid@outlook.com>
2025-04-17 03:09:10 -0500

    AES-25: Add examination schema and routes for creating, updating and getting examination details

--- core-server/src/routes/index.ts
+++ core-server/src/routes/index.ts
@@ -1,27 +1,34 @@
-import generatePresignedUrls from '@/routes/generate-presigned-urls'
-import getDoctorInfoRoute from '@/routes/get-doctor-info'
-import getEmrDetailsRoute from '@/routes/get-emr-details'
-import getAllEmrsFromPhone from '@/routes/get-erms-from-phone'
-import getPatientInfoRoute from '@/routes/get-patient-info'
-import getRedFlagsRoute from '@/routes/get-redflags'
-import homeRoute from '@/routes/home-page'
-import loginDoctorRoute from '@/routes/login-doctor'
-import patchEmrRoute from '@/routes/patch-emr-details'
-import registerDoctorRoute from '@/routes/register-doctor'
-import searchPatientsRoute from '@/routes/search-patients'
-import updateEmr from '@/routes/update-emr'
+import generatePresignedUrls from '@/routes/generate-presigned-urls';
+import getDoctorInfoRoute from '@/routes/get-doctor-info';
+import getEmrDetailsRoute from '@/routes/get-emr-details';
+import getAllEmrsFromPhone from '@/routes/get-erms-from-phone';
+import getExaminationDetailsRoute from '@/routes/get-examination-details';
+import getPatientInfoRoute from '@/routes/get-patient-info';
+import getRedFlagsRoute from '@/routes/get-redflags';
+import homeRoute from '@/routes/home-page';
+import loginDoctorRoute from '@/routes/login-doctor';
+import patchEmrRoute from '@/routes/patch-emr-details';
+import registerDoctorRoute from '@/routes/register-doctor';
+import searchPatientsRoute from '@/routes/search-patients';
+import updateEmr from '@/routes/update-emr';
+import updateExaminationDetailsRoute from '@/routes/update-examination-details';
+import createExaminationDetailsRoute from '@/routes/create-examination-details';
+
 export const registerRoutes = () => {
-	// hacky way to call the scripts and registering the routes
-	homeRoute.getRoutingPath()
-	loginDoctorRoute.getRoutingPath()
-	registerDoctorRoute.getRoutingPath()
-	generatePresignedUrls.getRoutingPath()
-	getEmrDetailsRoute.getRoutingPath()
-	getPatientInfoRoute.getRoutingPath()
-	getRedFlagsRoute.getRoutingPath()
-	patchEmrRoute.getRoutingPath()
-	getDoctorInfoRoute.getRoutingPath()
-	getAllEmrsFromPhone.getRoutingPath()
-	searchPatientsRoute.getRoutingPath()
-	updateEmr.getRoutingPath()
-}
+  // hacky way to call the scripts and registering the routes
+  homeRoute.getRoutingPath();
+  loginDoctorRoute.getRoutingPath();
+  registerDoctorRoute.getRoutingPath();
+  generatePresignedUrls.getRoutingPath();
+  getEmrDetailsRoute.getRoutingPath();
+  getPatientInfoRoute.getRoutingPath();
+  getRedFlagsRoute.getRoutingPath();
+  patchEmrRoute.getRoutingPath();
+  getDoctorInfoRoute.getRoutingPath();
+  getAllEmrsFromPhone.getRoutingPath();
+  searchPatientsRoute.getRoutingPath();
+  updateEmr.getRoutingPath();
+  getExaminationDetailsRoute.getRoutingPath();
+  updateExaminationDetailsRoute.getRoutingPath();
+  createExaminationDetailsRoute.getRoutingPath();
+};
