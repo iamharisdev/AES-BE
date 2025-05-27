34e574587a6cafa023ec86cb917fdb31f5ad54e3
say2alihassan <ali.hassan.sulehree@gmail.com>
2025-05-27 17:42:22 +0500

    AES-110-created the proposed plan table
    added id, followupdate, doctoreNotes, additional notes, advisedLabTests also added emr_id

---
+++ core-server/src/models/proposedPlan.ts
@@ -0,0 +1,11 @@
+// drizzle/schema.ts
+import { pgTable, serial, text, timestamp, jsonb ,   uuid} from "drizzle-orm/pg-core";
+
+export const followups = pgTable("proposed_plan", {
+  id: serial("id").primaryKey(),
+  emrId: uuid('emr_id').notNull(),
+  followupDate: timestamp("followup_date").notNull(),
+  doctorNotes: text("doctor_notes").notNull(),
+  additionalNotes: text("additional_notes"),
+  advisedLabTests: jsonb("advised_lab_tests").default([]).notNull(),
+});
